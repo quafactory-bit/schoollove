dea72ea06c519e34d14c3a5644b781aeef2f4409
quafactory-bit <quafactory@gmail.com>
2026-05-22 20:44:17 +0900

    fix: 네이버 서치어드바이저 메타태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '스쿨러브아이 - 학교 동창 인스타 찾기',
   description: '전국 초/중/고/대학교 동창들의 인스타그램을 연결해보세요',
+  other: {
+    'naver-site-verification': '73076bec23ea237533a3dcee3e0e9a27c743e249',
+  },
 }
 
 export default function RootLayout({
