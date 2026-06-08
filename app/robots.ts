0f11c42c9ec98428c197fc5cb3748e1e03dc8eaf
quafactory-bit <quafactory@gmail.com>
2026-06-08 15:03:01 +0900

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,5 @@
 import { MetadataRoute } from "next";
-
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://schoollove.kr";
-
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://www.schoollove.kr";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
